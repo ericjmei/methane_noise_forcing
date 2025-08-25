c3e8a4bd9c01349a05c43b8beaf8f542569cebf2
ericjmei <eric.mei2000@gmail.com>
2025-08-25 16:48:04 -0700

    added plotting submodule
    - added mscorefonts to pixi toml
    - created plotting.constants, which stores constants like colors and rcparams for matplotlib

---
+++ src/methane_noise_forcing/plotting/__init__.py
@@ -0,0 +1,9 @@
+# src/methane_noise_forcing/plotting/__init__.py
+# -*- coding: utf-8 -*-
+"""Functionality for generating plots."""
+
+from . import constants
+
+__all__ = [
+    "constants"
+]
---
+++ src/methane_noise_forcing/plotting/constants.py
@@ -0,0 +1,45 @@
+# src/methane_noise_forcing/plotting/constants.py
+# -*- coding: utf-8 -*-
+"""
+Constants (colors, settings) for plotting.
+"""
+
+# rcparams
+large_font = 8.5
+small_font = 7
+matplotlib_rcparams = {
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Arial"],
+    "font.size": large_font,
+    "axes.titlesize": large_font,
+    "axes.labelsize": large_font,
+    "xtick.labelsize": small_font,
+    "ytick.labelsize": small_font,
+    "legend.fontsize": small_font,
+}
+
+# figure width (inches)
+fig_width = {
+    "one_column": 3.42,
+    "two_column_large": 7
+}
+
+# colors (using bright and vibrant schemes from https://sronpersonalpages.nl/~pault/)
+colors_bright = {
+    "blue": "#4477AA",
+    "cyan": "#66CCEE",
+    "green": "#228833",
+    "yellow": "#CCBB44",
+    "red": "#EE6677",
+    "purple": "#AA3377",
+    "grey": "#BBBBBB",
+}
+colors_vibrant = {
+    "blue": "#0077BB",
+    "cyan": "#33BBEE",
+    "teal": "#009988",
+    "orange": "#EE7733",
+    "red": "#CC3311",
+    "magenta": "#EE3377",
+    "grey": "#BBBBBB",
+}
